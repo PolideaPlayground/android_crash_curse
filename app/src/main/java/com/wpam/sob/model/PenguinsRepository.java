97f59fc5ecd9531f73fb790a0b9bd4d5613e9b1a
Pawel Byszewski <>
2019-12-02 13:21:54 +0100

    penguins

---
+++ app/src/main/java/com/wpam/sob/model/PenguinsRepository.java
@@ -0,0 +1,64 @@
+package com.wpam.sob.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PenguinsRepository {
+
+    public List<String> fetchAll() {
+        return Arrays.asList(
+                "Adeli",
+                "antarktyczny",
+                "białobrewy",
+                "białoskrzydły",
+                "cesarski",
+                "grubodzioby",
+                "grzebieniasty",
+                "Humboldta",
+                "królewski",
+                "krótkoczuby",
+                "Magellana",
+                "mały",
+                "przylądkowy",
+                "równikowy",
+                "skalny",
+                "szczotkoczuby",
+                "złotoczuby",
+                "Adeli",
+                "antarktyczny",
+                "białobrewy",
+                "białoskrzydły",
+                "cesarski",
+                "grubodzioby",
+                "grzebieniasty",
+                "Humboldta",
+                "królewski",
+                "krótkoczuby",
+                "Magellana",
+                "mały",
+                "przylądkowy",
+                "równikowy",
+                "skalny",
+                "szczotkoczuby",
+                "złotoczuby",
+                "Adeli",
+                "antarktyczny",
+                "białobrewy",
+                "białoskrzydły",
+                "cesarski",
+                "grubodzioby",
+                "grzebieniasty",
+                "Humboldta",
+                "królewski",
+                "krótkoczuby",
+                "Magellana",
+                "mały",
+                "przylądkowy",
+                "równikowy",
+                "skalny",
+                "szczotkoczuby",
+                "złotoczuby",
+                "żółtooki"
+        );
+    }
+}
